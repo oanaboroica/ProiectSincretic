c29ebe757f9ec7bebea5bb1a922269f30fab4db2
oanaboroica <raluca.boroica@student.upt.ro>
2022-05-24 21:44:04 +0300

    Patrate-perfecte

--- ProiectSincretic.c
+++ ProiectSincretic.c
@@ -1,24 +1,28 @@
 #include<stdio.h>
 #include<string.h>
+#define MAX_NUM 100
 
-void randnum(int n)
+void generare(int n)
 {
-int random;
-srand(time(NULL));
-for(int i=0;i<n;i++)
-{
-random=rand();
-printf(" ", random);
-}
-printf("\n");
+	int random[100], sum=0;
+	srand(time(NULL));
+	
+	for (int i = 0; i < n; i++){
+		random[i] = rand()%MAX_NUM;
+		printf("%d ", random[i]);
+
+		if (sqrt(random[i]) == (int)(sqrt(random[i])))
+			sum = sum + 1;
+	}
+
+	printf("\n In sir exista %d patrate perfecte.", sum);
 }
 
 int main() {
-
-int n;
-printf("n= ");
-scanf("%d", &n);
-randnum(n);
+	int n;
+	printf("n= "); scanf("%d", &n);
+	printf("Numerele generate sunt:\n");
+	generare(n);
 
 	return 0;
 }
